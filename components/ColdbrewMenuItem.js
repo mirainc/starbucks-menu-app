b287ff966a7047cd6bc7f0aa46c17b55c234b3c6
Sebastian Key <sebastianwkey@gmail.com>
2022-07-28 14:03:16 +1000

    Styling

--- components/ColdbrewMenuItem.js
+++ components/ColdbrewMenuItem.js
@@ -1,15 +1,15 @@
-import { formatPrice, getTag } from "../utils";
-import cn from "classnames";
-import Image from "next/image";
-import styles from "../styles/coldbrewMenuItem.module.css";
+import { formatPrice, getTag } from '../utils';
+import cn from 'classnames';
+import Image from 'next/image';
+import styles from '../styles/coldbrewMenuItem.module.css';
 
 const ColdbrewMenuItems = ({ name, price, calories, inStock, tags }) => {
-  const itemId = getTag(tags, "id");
+  const itemId = getTag(tags, 'id');
 
   const tagImageMap = {
-    regular: "/cold-brew.webp",
-    "salted-caramel": "/cold-brew-salted-caramel.webp",
-    milk: "/cold-brew-milk.webp",
+    regular: '/cold-brew.webp',
+    'salted-caramel': '/cold-brew-salted-caramel.webp',
+    milk: '/cold-brew-milk.webp',
   };
 
   return (
@@ -17,8 +17,8 @@ const ColdbrewMenuItems = ({ name, price, calories, inStock, tags }) => {
       <div className={styles.imageContainer}>
         <Image src={tagImageMap[itemId]} alt="" width={220} height={220} />
       </div>
-      <div>
-        <div className={cn("title", inStock ? "" : " out-of-stock")}>
+      <div className={styles.text}>
+        <div className={cn('title', inStock ? '' : ' out-of-stock')}>
           {name}
         </div>
         <div className="subtitle">
--- components/Modifier.js
+++ components/Modifier.js
@@ -1,19 +1,19 @@
-import { formatPrice, getTag } from "../utils";
-import cn from "classnames";
+import { formatPrice, getTag } from '../utils';
+import cn from 'classnames';
 
-import styles from "../styles/menuItem.module.css";
+import styles from '../styles/modifier.module.css';
 
 const Modifier = (item) => {
   const { name, price } = item;
-  const calories = item.calories || getTag(item.tags, "calories");
+  const calories = item.calories || getTag(item.tags, 'calories');
 
   return (
     <div>
-      <div className={cn(styles.root, "h2")}>{name}</div>
+      <div className={cn(styles.root, 'h2')}>{name}</div>
       <div className={styles.description}>
         Blonde or Signature
         <div className={styles.details}>
-          {formatPrice(price)} {calories ? `| ${calories} Cal` : ""}
+          ${formatPrice(price)} {calories ? `| ${calories} Cal` : ''}
         </div>
       </div>
     </div>
