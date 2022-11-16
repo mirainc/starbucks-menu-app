117a76319df349250e8dd0c53114fd163131ee4d
Jordan <github@jschr.io>
2022-11-16 14:30:16 -0500

    Offline support

--- next.config.js
+++ next.config.js
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=1",
+          },
+          {
+            key: 'x-hello',
+            value: 'world',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
