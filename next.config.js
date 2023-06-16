a437c3fc1df5b453c2619abe83735d50b1ea3a1a
Jordan <github@jschr.io>
2023-06-16 13:17:58 -0400

    Test without headers

--- next.config.js
+++ next.config.js
@@ -1,27 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'X-Accel-Expires',
-            value: '1',
-          },
-        ],
-      },
-      {
-        source: '/_next/data/:path*.json',
-        headers: [
-          {
-            key: 'X-Accel-Expires',
-            value: '1',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       headers: [{ key: 'X-Accel-Expires', value: '1' }],
+  //     },
+  //     {
+  //       source: '/_next/data/:path*.json',
+  //       headers: [{ key: 'X-Accel-Expires', value: '1' }],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
