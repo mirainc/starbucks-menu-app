efb15ffe89b77e79ae25908835173706013f6656
Jordan Schroter <github@jschr.io>
2024-05-24 13:27:11 -0400

    Set Cache Control header on assets (#6)
    * Set minimumCacheTTL
    
    * Try setting cache control on images
    
    * Test img
    
    * Use next/image

--- next.config.js
+++ next.config.js
@@ -3,17 +3,14 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/',
         headers: [
           {
             key: 'Cache-Control',
-            value: 'max-age=1',
-          },
-          {
-            key: 'x-test',
-            value: '1',
+            value: 'public, max-age=31536000, immutable',
           },
         ],
+        source:
+          '/:path(.+\\.(?:ico|png|svg|jpg|jpeg|avif|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)',
       },
     ];
   },
